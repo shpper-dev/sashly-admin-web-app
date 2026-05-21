69538e74d5aaaa0298e42e65fae32719560d6f1e
fadwa <fadwaabdulkader79@gmail.com>
2026-05-21 12:09:30 +0400

    fixing metrics layout

--- app/metrics/layout.tsx
+++ app/metrics/layout.tsx
@@ -1,12 +1,15 @@
 
 import MetricsSidebar from '@/components/metrics/MetricsSideBar';
+import { ChatNotificationProvider } from '@/lib/providers/ChatNotificationProvider';
 import React from 'react';
 
 export default function layout({children}:Readonly<{children:React.ReactNode}>) {
   return (
     <div>
-        <MetricsSidebar />
-        {children}
+        <ChatNotificationProvider>
+          <MetricsSidebar />
+           {children}
+        </ChatNotificationProvider>
     </div>
   )
 }
